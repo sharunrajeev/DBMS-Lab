f40e420b9f8aed24b211ffba093d8a42ec42c13d
Sharun <sharunpublic@gmail.com>
2021-10-12 23:58:29 +0530

    Update Taskmanager

--- Question 2/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $mysqli = new mysqli("localhost", "sharun", "sharun2001", "taskmanager");
-
-    // Check connection
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-        exit();
-    }
-?>
--- Question 2/tm.php
+++ Question 2/tm.php
@@ -1,24 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Manager</title>
+    <style>
+        table,
+        tr,
+        td,
+        th {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+
+        input {
+            margin-top: 10px;
+        }
+    </style>
 </head>
+
 <body>
     <?php
-        require("db.php");
-        exec("ps aux | head -n 1", $header);
-        exec("ps aux | tail -n 2", $output);
-        echo "$header[0]" . "<br>";
-        foreach ($output as $line) {
-            foreach (explode(" ", $line) as $word) {
-                echo "$word ";
-            }
-            echo "<br>";
+    // header("refresh: 20");
+
+    $words = array();
+    $mysqli = new mysqli("localhost", "sharun", "sharun2001", "taskmanager");
+
+    // Check connection
+    if ($mysqli->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+        exit();
+    }
+
+    // Trucate all the rows
+    $mysqli->query("TRUNCATE tasks");
+
+    exec("ps aux | tail -n 10", $output);
+    foreach ($output as $line) {
+        $words = preg_split("/\s+/", $line);
+        $mysqli->query("INSERT INTO tasks (USER,PID,CPU,MEM,VSZ,RSS,TTY,STAT,START,TIME,COMMAND) VALUES('$words[0]','$words[1]','$words[2]','$words[3]','$words[4]','$words[5]','$words[6]','$words[7]','$words[8]','$words[9]','$words[10]')");
+    }
+
+    // Displaying from DB
+    echo '<form action="" method="POST">';
+    echo '<table>';
+    echo '<tr><th>KILL</th><th>USER</th><th>PID</th><th>CPU%</th><th>MEM%</th><th>VSZ</th><th>RSS</th><th>TTY</th><th>STAT</th><th>START</th><th>TIME</th><th>COMMAND</th></tr>';
+    $result = $mysqli->query("SELECT * FROM tasks");
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo '<td><input type="checkbox" name="kill[]" value='.$row['PID'].'></td>';
+            echo "<td>$row[USER]</td>";
+            echo "<td>$row[PID]</td>";
+            echo "<td>$row[CPU]</td>";
+            echo "<td>$row[MEM]</td>";
+            echo "<td>$row[VSZ]</td>";
+            echo "<td>$row[RSS]</td>";
+            echo "<td>$row[TTY]</td>";
+            echo "<td>$row[STAT]</td>";
+            echo "<td>$row[START]</td>";
+            echo "<td>$row[TIME]</td>";
+            echo "<td>$row[COMMAND]</td>";
+            echo "</tr>";
         }
+    }
+    echo "</table>";
+
+    // Submit button
+    echo "<input type='submit' value='Submit' name='submit'>";
+    echo "</form>";
+    if (isset($_POST['submit'])) {
+        if (!empty($_POST['kill'])) {
+            foreach ($_POST['kill'] as $checked) {
+                exec("kill -9".$checked, $unknown);
+                echo implode("<br>",$unknown);
+            }
+        }
+    }
+
+    // Refresh button
+    echo '<form action="" method="POST">';
+    echo "<input type='submit' value='Refresh' name='refresh'>";
+    echo "</form>";
+    if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['refresh'])) {
+        header("refresh");
+    }
+
+
 
     ?>
 </body>
+
 </html>
